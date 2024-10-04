fd4fe5a3e0990f9ea8e32f28488f1c48776702c4
Laptop <laptop@disroot.org>
2024-10-05 01:20:15 +0300

    hm

--- go.mod
+++ go.mod
@@ -2,10 +2,7 @@ module github.com/maid-zone/website
 
 go 1.21.3
 
-require (
-	github.com/gofiber/fiber/v2 v2.52.5
-	github.com/json-iterator/go v1.1.12
-)
+require github.com/gofiber/fiber/v2 v2.52.5
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
@@ -14,11 +11,7 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
-	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/philhofer/fwd v1.1.2 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
-	github.com/tinylib/msgp v1.1.8 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
--- main.go
+++ main.go
@@ -3,56 +3,45 @@ package main
 import (
 	_ "embed"
 	"log"
-	"net/url"
-	"os"
-	"regexp"
 	"strings"
-	"sync"
-	"time"
-
-	"math/rand"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/compress"
 	"github.com/gofiber/fiber/v2/middleware/earlydata"
-	"github.com/gofiber/fiber/v2/middleware/limiter"
 	"github.com/gofiber/fiber/v2/middleware/recover"
-	"github.com/maid-zone/website/mut"
-
-	jsoniter "github.com/json-iterator/go"
 )
 
-var alphanum = [62]rune{'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
+// var alphanum = [62]rune{'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
 
-func RandomSlug(length int) string {
-	b := make([]rune, length)
-	for i := range b {
-		b[i] = alphanum[rand.Intn(len(alphanum))]
-	}
+// func RandomSlug(length int) string {
+// 	b := make([]rune, length)
+// 	for i := range b {
+// 		b[i] = alphanum[rand.Intn(len(alphanum))]
+// 	}
 
-	return string(b)
-}
+// 	return string(b)
+// }
 
-var isSavingShortened bool
+// var isSavingShortened bool
 
-func saveShortened() {
-	if isSavingShortened {
-		return
-	}
-	isSavingShortened = true
+// func saveShortened() {
+// 	if isSavingShortened {
+// 		return
+// 	}
+// 	isSavingShortened = true
 
-	raw, err := json.Marshal(shortened)
-	if err != nil {
-		isSavingShortened = false
-		panic(err)
-	}
+// 	raw, err := json.Marshal(shortened)
+// 	if err != nil {
+// 		isSavingShortened = false
+// 		panic(err)
+// 	}
 
-	err = os.WriteFile(data+"shortened.json", raw, 0666)
-	isSavingShortened = false
-	if err != nil {
-		panic(err)
-	}
-}
+// 	err = os.WriteFile(data+"shortened.json", raw, 0666)
+// 	isSavingShortened = false
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
 
 //go:embed assets/index.html
 var html []byte
@@ -60,38 +49,39 @@ var html []byte
 //go:embed assets/index.ans
 var ansi []byte
 
-type Shortened struct {
-	SlugToLink       mut.Map[string, string]
-	LinkToRandomSlug mut.Map[string, string]
-}
+// type Shortened struct {
+// 	SlugToLink       mut.Map[string, string]
+// 	LinkToRandomSlug mut.Map[string, string]
+// }
 
-var shortened = Shortened{
-	SlugToLink: mut.Map[string, string]{
-		Map: make(map[string]string),
-		Mut: &sync.RWMutex{},
-	},
-	LinkToRandomSlug: mut.Map[string, string]{
-		Map: make(map[string]string),
-		Mut: &sync.RWMutex{},
-	},
-}
-var slugre = regexp.MustCompile(`^[a-zA-Z0-9]{1,256}$`)
-var json = jsoniter.ConfigFastest
+//	var shortened = Shortened{
+//		SlugToLink: mut.Map[string, string]{
+//			Map: make(map[string]string),
+//			Mut: &sync.RWMutex{},
+//		},
+//		LinkToRandomSlug: mut.Map[string, string]{
+//			Map: make(map[string]string),
+//			Mut: &sync.RWMutex{},
+//		},
+//	}
+//
+// var slugre = regexp.MustCompile(`^[a-zA-Z0-9]{1,256}$`)
+// var json = jsoniter.ConfigFastest
 
-const data = "/data/"
+// const data = "/data/"
 
 func main() {
-	go func() {
-		raw, err := os.ReadFile(data + "shortened.json")
-		if err != nil {
-			panic(err)
-		}
+	// go func() {
+	// 	raw, err := os.ReadFile(data + "shortened.json")
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
 
-		err = json.Unmarshal(raw, &shortened)
-		if err != nil {
-			panic(err)
-		}
-	}()
+	// 	err = json.Unmarshal(raw, &shortened)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// }()
 	app := fiber.New()
 	app.Use(compress.New())
 	app.Use(recover.New())
@@ -130,63 +120,63 @@ func main() {
 		return c.SendFile("assets/s.html", true)
 	})
 
-	app.Get("/s/:slug", func(c *fiber.Ctx) error {
-		slug := c.Params("slug")
-		val, ok := shortened.SlugToLink.GetValueAndState(slug)
-		if !ok {
-			return fiber.ErrNotFound
-		}
+	// app.Get("/s/:slug", func(c *fiber.Ctx) error {
+	// 	slug := c.Params("slug")
+	// 	val, ok := shortened.SlugToLink.GetValueAndState(slug)
+	// 	if !ok {
+	// 		return fiber.ErrNotFound
+	// 	}
 
-		return c.Redirect(val)
-	})
+	// 	return c.Redirect(val)
+	// })
 
-	app.Get("/api/shorten", limiter.New(limiter.Config{SkipFailedRequests: true, Expiration: time.Second, Max: 2, LimiterMiddleware: limiter.SlidingWindow{}}), func(c *fiber.Ctx) error {
-		s := c.Query("url")
-		if s == "" || len(s) > 256 {
-			return fiber.ErrBadRequest
-		}
+	// app.Get("/api/shorten", limiter.New(limiter.Config{SkipFailedRequests: true, Expiration: time.Second, Max: 2, LimiterMiddleware: limiter.SlidingWindow{}}), func(c *fiber.Ctx) error {
+	// 	s := c.Query("url")
+	// 	if s == "" || len(s) > 256 {
+	// 		return fiber.ErrBadRequest
+	// 	}
 
-		if parsed, err := url.Parse(s); err != nil || parsed.Scheme == "" {
-			return fiber.ErrBadRequest
-		}
+	// 	if parsed, err := url.Parse(s); err != nil || parsed.Scheme == "" {
+	// 		return fiber.ErrBadRequest
+	// 	}
 
-		slug := strings.Trim(c.Query("slug"), " ")
-		if !slugre.MatchString(slug) && slug != "" {
-			return fiber.ErrBadRequest
-		}
+	// 	slug := strings.Trim(c.Query("slug"), " ")
+	// 	if !slugre.MatchString(slug) && slug != "" {
+	// 		return fiber.ErrBadRequest
+	// 	}
 
-		if _, ok := shortened.SlugToLink.GetValueAndState(slug); ok {
-			return c.Status(fiber.StatusConflict).SendString("slug taken")
-		}
+	// 	if _, ok := shortened.SlugToLink.GetValueAndState(slug); ok {
+	// 		return c.Status(fiber.StatusConflict).SendString("slug taken")
+	// 	}
 
-		if slug == "" {
-			if slug, ok := shortened.LinkToRandomSlug.GetValueAndState(s); ok {
-				return c.SendString(slug)
-			}
+	// 	if slug == "" {
+	// 		if slug, ok := shortened.LinkToRandomSlug.GetValueAndState(s); ok {
+	// 			return c.SendString(slug)
+	// 		}
 
-			for i := 0; i < 6; i++ {
-				if i == 5 {
-					return fiber.ErrInternalServerError // fuck it, we're out of luck this time
-				}
+	// 		for i := 0; i < 6; i++ {
+	// 			if i == 5 {
+	// 				return fiber.ErrInternalServerError // fuck it, we're out of luck this time
+	// 			}
 
-				slug = RandomSlug(6)
-				if _, ok := shortened.SlugToLink.GetValueAndState(slug); !ok {
-					cls := strings.Clone(slug)
-					clu := strings.Clone(s)
+	// 			slug = RandomSlug(6)
+	// 			if _, ok := shortened.SlugToLink.GetValueAndState(slug); !ok {
+	// 				cls := strings.Clone(slug)
+	// 				clu := strings.Clone(s)
 
-					shortened.LinkToRandomSlug.Set(clu, cls)
-					shortened.SlugToLink.Set(cls, clu)
-					log.Printf("shortened %s -> %s\n", s, slug)
-					return c.SendString(slug)
-				}
-			}
-		}
+	// 				shortened.LinkToRandomSlug.Set(clu, cls)
+	// 				shortened.SlugToLink.Set(cls, clu)
+	// 				log.Printf("shortened %s -> %s\n", s, slug)
+	// 				return c.SendString(slug)
+	// 			}
+	// 		}
+	// 	}
 
-		shortened.SlugToLink.Set(strings.Clone(slug), strings.Clone(s))
-		go saveShortened()
-		log.Printf("shortened %s -> %s\n", s, slug)
-		return c.SendString(slug)
-	})
+	// 	shortened.SlugToLink.Set(strings.Clone(slug), strings.Clone(s))
+	// 	go saveShortened()
+	// 	log.Printf("shortened %s -> %s\n", s, slug)
+	// 	return c.SendString(slug)
+	// })
 
 	app.Static("/", "assets", fiber.Static{Compress: true, MaxAge: 3600})
 
